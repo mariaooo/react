8a04c96685b187266c928bfea15f6cf256ab2fcf
KanJJ <2669511192@qq.com>
2017-11-21 21:45:49 +0800

    delete no use

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+ReactDOM.render(
+        /**注释 */
+        <div>
+          {/*it is a test*/}
+          <h1 >{1+1}</h1>
+          <div>it is just begin!</div>
+        </div>
+        ,
+        document.getElementById('root')
+);
