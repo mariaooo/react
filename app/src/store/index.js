43b4f4102b16f0b005ac012ae6cf4074125b3293
Chris Chen <smilechris@yeah.net>
2018-03-23 16:34:29 +0800

    Update index.js

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,21 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import createHistory from 'history/createBrowserHistory';
-import { routerMiddleware } from 'react-router-redux'
+import { routerMiddleware } from 'react-router-redux';
 import reducers from '../reducers/index';
 import setupSaga from '../sagas/index';
-import logger from '../utils/logger';
-import crashReporter from '../utils/crashReporter';
+import { crashReporter, logger, sessionExtend } from '../utils';
 
 
 // Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory();
 
 // Build the middleware for intercepting and dispatching navigation actions
-const historyMiddleware = routerMiddleware(history);
 
 const sagaMiddleware = createSagaMiddleware();
-let middlewares = [sagaMiddleware, historyMiddleware];
+let middlewares = [sagaMiddleware, sessionExtend];
 
 if (process.env.NODE_ENV !== 'production') {
   middlewares = [...middlewares, logger, crashReporter];
