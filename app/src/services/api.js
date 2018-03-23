8644ab9bf986d7d030ab7cd162be832be6e979a7
Chris Chen <smilechris@yeah.net>
2018-03-23 16:47:42 +0800

    Create api.js

---
+++ app/src/services/api.js
@@ -0,0 +1,13 @@
+
+import 'whatwg-fetch';
+import request from './request';
+import apiConfig from './apiConfig';
+
+
+const api = {
+  /*getsomeapi = () => {
+    // there have path 
+  }*/
+};
+
+export default api;
